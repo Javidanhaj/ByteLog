64e13552b590e6d0231c81bbbad5ddd6f9092d9e
Javidan Hajizada <76992705+Javidanhaj@users.noreply.github.com>
2025-08-19 18:12:46 +0400

    added method for optimization

--- src/main/java/com/javidanhaj/bytelog/repositories/TagRepository.java
+++ src/main/java/com/javidanhaj/bytelog/repositories/TagRepository.java
@@ -1,11 +1,17 @@
 package com.javidanhaj.bytelog.repositories;
 
+import com.javidanhaj.bytelog.domain.entities.Category;
 import com.javidanhaj.bytelog.domain.entities.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface TagRepository extends JpaRepository<Tag, UUID> {
+
+    @Query("SELECT t FROM Tag t LEFT JOIN FETCH  t.posts")
+    List<Tag> findAllWithPostCount();
 }
