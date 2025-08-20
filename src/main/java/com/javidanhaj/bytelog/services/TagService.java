03b4f5ce3ae829a93e560eeb1bd686425b55c022
Javidan Hajizada <76992705+Javidanhaj@users.noreply.github.com>
2025-08-21 01:04:10 +0400

    added tag create method

--- src/main/java/com/javidanhaj/bytelog/services/TagService.java
+++ src/main/java/com/javidanhaj/bytelog/services/TagService.java
@@ -4,7 +4,9 @@ package com.javidanhaj.bytelog.services;
 import com.javidanhaj.bytelog.domain.entities.Tag;
 
 import java.util.List;
+import java.util.Set;
 
 public interface TagService {
     List<Tag> getTags();
+    List<Tag> createTags(Set<String> tagNames);
 }
