99e98c1bf44acc2d8a0ca5d4b29a8c5249a81d8f
Javidan Hajizada <76992705+Javidanhaj@users.noreply.github.com>
2025-08-21 13:36:05 +0400

    added new methods' implementations

---
+++ src/main/java/com/javidanhaj/bytelog/services/impl/PostServiceImpl.java
@@ -0,0 +1,51 @@
+package com.javidanhaj.bytelog.services.impl;
+
+import com.javidanhaj.bytelog.domain.PostStatus;
+import com.javidanhaj.bytelog.domain.entities.Category;
+import com.javidanhaj.bytelog.domain.entities.Post;
+import com.javidanhaj.bytelog.domain.entities.Tag;
+import com.javidanhaj.bytelog.repositories.PostRepository;
+import com.javidanhaj.bytelog.services.CategoryService;
+import com.javidanhaj.bytelog.services.PostService;
+import com.javidanhaj.bytelog.services.TagService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class PostServiceImpl implements PostService {
+
+    private final PostRepository postRepository;
+    private final CategoryService categoryService;
+    private final TagService tagService;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Post> getAllPosts(UUID categoryId, UUID tagId) {
+        if(categoryId != null && tagId != null) {
+            Category category = categoryService.getCategoryById(categoryId);
+            Tag tag = tagService.getTagById(tagId);
+            postRepository.findAllByStatusAndCategoryAndTagsContaining(
+                    PostStatus.PUBLISHED,
+                    category,
+                    tag
+            );
+        }
+
+        if (categoryId != null) {
+            Category category = categoryService.getCategoryById(categoryId);
+            return postRepository.findAllByStatusAndCategory(PostStatus.PUBLISHED, category);
+        }
+
+        if (tagId != null) {
+            Tag tag = tagService.getTagById(tagId);
+            return postRepository.findAllByStatusAndTagsContaining(PostStatus.PUBLISHED, tag);
+        }
+
+        return postRepository.findAllByStatus(PostStatus.PUBLISHED);
+    }
+}
