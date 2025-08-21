2355bbf0c6588a48a6da065346667eeccf5598a6
Javidan Hajizada <76992705+Javidanhaj@users.noreply.github.com>
2025-08-21 13:37:08 +0400

    added new method implementations of TagService

--- src/main/java/com/javidanhaj/bytelog/services/impl/TagServiceImpl.java
+++ src/main/java/com/javidanhaj/bytelog/services/impl/TagServiceImpl.java
@@ -3,14 +3,12 @@ package com.javidanhaj.bytelog.services.impl;
 import com.javidanhaj.bytelog.domain.entities.Tag;
 import com.javidanhaj.bytelog.repositories.TagRepository;
 import com.javidanhaj.bytelog.services.TagService;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -46,5 +44,22 @@ public class TagServiceImpl implements TagService {
 
         savedTags.addAll(existingTags);
         return savedTags;
+    }
+
+    @Transactional
+    @Override
+    public void deleteTag(UUID id) {
+        tagRepository.findById(id).ifPresent(tag -> {
+            if(!tag.getPosts().isEmpty()){
+                throw new IllegalStateException("Cannot delete tag with posts");
+            }
+            tagRepository.deleteById(id);
+        });
+    }
+
+    @Override
+    public Tag getTagById(UUID id) {
+        return  tagRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Tag not found with id" + id));
     }
 }
