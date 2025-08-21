f84146b0013380b312ef83701f3c98ad69e380e6
Javidan Hajizada <76992705+Javidanhaj@users.noreply.github.com>
2025-08-22 00:28:06 +0400

    added draft endpoint

--- src/main/java/com/javidanhaj/bytelog/controllers/PostController.java
+++ src/main/java/com/javidanhaj/bytelog/controllers/PostController.java
@@ -2,14 +2,13 @@ package com.javidanhaj.bytelog.controllers;
 
 import com.javidanhaj.bytelog.domain.dtos.PostDto;
 import com.javidanhaj.bytelog.domain.entities.Post;
+import com.javidanhaj.bytelog.domain.entities.User;
 import com.javidanhaj.bytelog.mappers.PostMapper;
 import com.javidanhaj.bytelog.services.PostService;
+import com.javidanhaj.bytelog.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -21,6 +20,7 @@ public class PostController {
 
     private final PostService postService;
     private final PostMapper postMapper;
+    private final UserService userService;
 
     @GetMapping
     public ResponseEntity<List<PostDto>> getAllPosts(
@@ -29,6 +29,15 @@ public class PostController {
 
         List<Post> posts = postService.getAllPosts(categoryId, tagId);
         List<PostDto> postDtos = posts.stream().map(postMapper::toDto).toList();
+
+        return ResponseEntity.ok(postDtos);
+    }
+
+    @GetMapping(path = "/drafts")
+    public ResponseEntity<List<PostDto>> getDrafts(@RequestAttribute UUID userID){
+        User loggedInUser = userService.getUserByID(userID);
+        List<Post> draftPosts = postService.getDraftPosts(loggedInUser);
+        List<PostDto> postDtos = draftPosts.stream().map(postMapper::toDto).toList();
 
         return ResponseEntity.ok(postDtos);
     }
