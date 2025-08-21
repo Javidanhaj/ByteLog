46abd88a9d270d48ec28943f784b1cb4181195f0
Javidan Hajizada <76992705+Javidanhaj@users.noreply.github.com>
2025-08-21 13:34:41 +0400

    added new Error Handler

--- src/main/java/com/javidanhaj/bytelog/controllers/ErrorController.java
+++ src/main/java/com/javidanhaj/bytelog/controllers/ErrorController.java
@@ -1,6 +1,7 @@
 package com.javidanhaj.bytelog.controllers;
 
 import com.javidanhaj.bytelog.domain.dtos.ApiErrorResponse;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -49,5 +50,14 @@ public class ErrorController {
                 .message(ex.getMessage()) //This shouldn't expose internal messages,
                 .build();                 // but I used it like this for development, so this must be changed
         return new ResponseEntity<>(error, HttpStatus.UNAUTHORIZED );
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ApiErrorResponse> handleEntityNotFoundException(EntityNotFoundException ex){
+        ApiErrorResponse error = ApiErrorResponse.builder()
+                .status(HttpStatus.NOT_FOUND.value())
+                .message(ex.getMessage()) //This shouldn't expose internal messages,
+                .build();                 // but I used it like this for development, so this must be changed
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND );
     }
 }
