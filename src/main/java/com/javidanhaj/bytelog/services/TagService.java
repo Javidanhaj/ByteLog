56e2ba743d650ed56a352e8ae956ccd8980c5e2c
Javidan Hajizada <76992705+Javidanhaj@users.noreply.github.com>
2025-08-21 13:37:04 +0400

    added new methods

--- src/main/java/com/javidanhaj/bytelog/services/TagService.java
+++ src/main/java/com/javidanhaj/bytelog/services/TagService.java
@@ -5,8 +5,11 @@ import com.javidanhaj.bytelog.domain.entities.Tag;
 
 import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 
 public interface TagService {
     List<Tag> getTags();
     List<Tag> createTags(Set<String> tagNames);
+    void deleteTag(UUID id);
+    Tag getTagById(UUID id);
 }
