ee04eec033657bd4f399aab06a3a93cc03ed3334
Javidan Hajizada <76992705+Javidanhaj@users.noreply.github.com>
2025-08-21 13:35:30 +0400

    added new methods for Post Repo

--- src/main/java/com/javidanhaj/bytelog/repositories/PostRepository.java
+++ src/main/java/com/javidanhaj/bytelog/repositories/PostRepository.java
@@ -1,11 +1,19 @@
 package com.javidanhaj.bytelog.repositories;
 
+import com.javidanhaj.bytelog.domain.PostStatus;
+import com.javidanhaj.bytelog.domain.entities.Category;
 import com.javidanhaj.bytelog.domain.entities.Post;
+import com.javidanhaj.bytelog.domain.entities.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, UUID> {
+    List<Post> findAllByStatusAndCategoryAndTagsContaining(PostStatus status, Category category, Tag tag);
+    List<Post> findAllByStatusAndCategory(PostStatus status, Category category);
+    List<Post> findAllByStatusAndTagsContaining(PostStatus status, Tag tag);
+    List<Post> findAllByStatus(PostStatus status);
 }
