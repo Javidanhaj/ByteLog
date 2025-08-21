66da5d4a2afc3695b4c053c64fc4cbd702e9c331
Javidan Hajizada <76992705+Javidanhaj@users.noreply.github.com>
2025-08-21 13:35:01 +0400

    added Post Controller

---
+++ src/main/java/com/javidanhaj/bytelog/controllers/PostController.java
@@ -0,0 +1,35 @@
+package com.javidanhaj.bytelog.controllers;
+
+import com.javidanhaj.bytelog.domain.dtos.PostDto;
+import com.javidanhaj.bytelog.domain.entities.Post;
+import com.javidanhaj.bytelog.mappers.PostMapper;
+import com.javidanhaj.bytelog.services.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(path = "/api/v1/posts")
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostService postService;
+    private final PostMapper postMapper;
+
+    @GetMapping
+    public ResponseEntity<List<PostDto>> getAllPosts(
+            @RequestParam(required = false) UUID categoryId,
+            @RequestParam(required = false) UUID tagId) {
+
+        List<Post> posts = postService.getAllPosts(categoryId, tagId);
+        List<PostDto> postDtos = posts.stream().map(postMapper::toDto).toList();
+
+        return ResponseEntity.ok(postDtos);
+    }
+}
